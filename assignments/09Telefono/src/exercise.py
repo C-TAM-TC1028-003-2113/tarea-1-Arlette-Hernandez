20c22d7aeea7c9c209d03580853603fe044600a6
Arlette <a00834620@itesm.com>
2021-08-29 16:49:53 -0500

    Termine el ejercicio 2

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -2,17 +2,14 @@ def main():
     # escribe tu código abajo de esta línea
     msj=int(input("Dame el número de mensajes: "))
     m=float(input("Dame el número de megas: "))
-    min=int(input("Dame el numero de minutos¨: "))
+    minutos=int(input("Dame el numero de minutos¨: "))
 
     mensajes=0.80*msj
     megas=0.80*m
-    minutos=0.80*min
+    min=0.80*minutos
 
-    costomensual= mensajes+megas+minutos
-    print("El costo mensual es:",mensualidad)
-
-
-
+    costomensual= mensajes+megas+min
+    print("El costo mensual es:",costomensual)
 
 if __name__ == '__main__':
     main()

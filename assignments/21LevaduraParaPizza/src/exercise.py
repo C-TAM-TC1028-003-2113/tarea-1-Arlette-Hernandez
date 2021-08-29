97e2b423918615e846a50d2f8f94042654e46b2e
Arlette <a00834620@itesm.com>
2021-08-29 17:51:41 -0500

    Termine el ejercicio 6

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    gramosharina= float(input("Dame la harina en gramos: "))
+    kilo=50
+    mediokilo=25
+    gramoslevadura= kilo+mediokilo
+    print("El total de levadura que se necesita es:",gramoslevadura)
+
 
 
 if __name__ == '__main__':

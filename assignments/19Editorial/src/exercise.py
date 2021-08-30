dd634d90d6982c508b646e17ac42e5e667f11bdd
Arlette <a00834620@itesm.com>
2021-08-29 19:28:25 -0500

    Termine el ejercicio 4

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,17 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    palabras= int(input("Dame el número de palabras: "))
+    descuento=0.012
+    restodeldescuento=9
+    entero=100
+    costoporpalbra=60/500
+    costo= descuento*restodeldescuento
+    costofinal=costo*entero
+    print("El costo del descuento es:,costofinal")
+
+
+
+
 
 
 if __name__ == '__main__':

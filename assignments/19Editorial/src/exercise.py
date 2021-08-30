a7529a96618a55fb59adcaee3b239ce1b4fdeab7
Arlette <a00834620@itesm.com>
2021-08-29 19:44:21 -0500

    Termine el ejercicio 6

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,13 +1,14 @@
 def main():
     # escribe tu código abajo de esta línea
     palabras= int(input("Dame el número de palabras: "))
-    descuento=0.012
+    descuento=0.10
     restodeldescuento=9
-    entero=100
-    costoporpalbra=60/500
-    costo= descuento*restodeldescuento
+    entero=1000
+    costoporpalabra=60/500
+    descuentofinal= costoporpalabra*descuento
+    costo=descuentofinal*restodeldescuento
     costofinal=costo*entero
-    print("El costo del descuento es:,costofinal")
+    print("El costo del descuento es:",costofinal)
 
 
 
